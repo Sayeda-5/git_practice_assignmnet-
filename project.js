0a2b8cc6a4b2723a16ad9c7dc44b2986cb6a5261
Satya Indra Dev <112754841+SatyaIndraDev@users.noreply.github.com>
2022-09-13 18:27:03 +0530

    Update project.js

--- project.js
+++ project.js
@@ -1,26 +1,27 @@
-//Problem 1 :** Check whether a number is Prime or not
+//Check whether a number is Prime or not
 
-let num=13;
-Let fac=0;
-for(let a=0;a<num;a++){
- if(num%num==0){
-  fac++;
-   }
-}
 
-fac==2?console.log("Yes") : console.log("No");
-
-//Problem 2 : Check whether a string is palindrome or not.
-
-Let srt="masai";
-
-for(let a=0;a<str.length;a++){
-  for(let i=str.length-1;i>=0;i++){
-    if(str[a]===str[i]){
-     console.log("Yes");
-   }
-    else{
-     console.log("No");
+ let num=29;
+     let fac=0;
+     
+    for(let a=1;a<=num;a++){
+        if(num%a==0){
+           fac++;
+        }
     }
-  }
-}
+    
+  fac===2 ? console.log("Yes", num, "is Prime") : console.log("No", num, "is not Prime");
+    
+     
+  
+    //Check whether a string is palindrome or not.
+    
+    let str="masai";
+    
+    let rev="";
+    
+    for(let a=str.length-1;a>=0;a--){
+        rev+=str[a];
+    }
+    
+   str===rev ?console.log("Yes"): console.log("No") 
