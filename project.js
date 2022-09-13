0d7582d446a159a6c8b86c62e4934575f63cd9a3
Sidra Syed <107279050+Sayeda-5@users.noreply.github.com>
2022-09-13 16:43:31 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,26 @@
+//Problem 1 :** Check whether a number is Prime or not
+
+let num=13;
+Let fac=0;
+for(let a=0;a<num;a++){
+ if(num%num==0){
+  fac++;
+   }
+}
+
+fac==2?console.log("Yes") : console.log("No");
+
+//Problem 2 : Check whether a string is palindrome or not.
+
+Let srt="masai";
+
+for(let a=0;a<str.length;a++){
+  for(let i=str.length-1;i>=0;i++){
+    if(str[a]===str[i]){
+     console.log("Yes");
+   }
+    else{
+     console.log("No");
+    }
+  }
+}
